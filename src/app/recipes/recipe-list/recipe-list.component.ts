9cc1aa6b280f19133cc0ac08b4c94308851b6105
Amar <amar3232324@gmail.com>
2022-01-13 11:47:39 +0530

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe 1", "This is a test recipe", "https://cookwithparul.com/wp-content/uploads/2021/11/Eggless-Omelette-Recipe.jpg"),
+    new Recipe("Test Recipe 2", "This is another test recipe", "https://eadn-wc02-3894996.nxedge.io/cdn/wp-content/uploads/2018/01/pistachio-turmeric-rice-bowl6-1024x683.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
