078a13ae93b339161ee6f51c4a07721209f35a7a
Amar <amar3232324@gmail.com>
2022-01-17 21:54:01 +0530

    Databinding added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe("Test Recipe 1", "This is a test recipe", "https://cookwithparul.com/wp-content/uploads/2021/11/Eggless-Omelette-Recipe.jpg"),
     new Recipe("Test Recipe 2", "This is another test recipe", "https://eadn-wc02-3894996.nxedge.io/cdn/wp-content/uploads/2018/01/pistachio-turmeric-rice-bowl6-1024x683.jpg")
@@ -15,6 +17,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
