2b75031f76af3a11d09a963604104cdbeaa24f34
Amar <amar3232324@gmail.com>
2022-01-23 19:15:44 +0530

    services added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,19 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe("Test Recipe 1", "This is a test recipe", "https://cookwithparul.com/wp-content/uploads/2021/11/Eggless-Omelette-Recipe.jpg"),
-    new Recipe("Test Recipe 2", "This is another test recipe", "https://eadn-wc02-3894996.nxedge.io/cdn/wp-content/uploads/2018/01/pistachio-turmeric-rice-bowl6-1024x683.jpg")
-  ];
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,36 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+
+export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe("Chicken Biryani",
+     "Most sought out dish in India",
+      "https://i.tribune.com.pk/media/images/1590373-biryani-1513939158/1590373-biryani-1513939158.gif",
+      [new Ingredient('rice', 1),
+      new Ingredient('chicken', 2)]
+    ),
+    new Recipe("Burger",
+     "Take away food", 
+     "https://www3.pictures.zimbio.com/mp/wwj1hrnad01x.jpg",
+      [new Ingredient('bread', 1),
+      new Ingredient('meat slice', 1)]
+    )
+  ];
+  
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
